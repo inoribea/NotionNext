0eec8d5c8234d986eb1a38d2d4152dba673f467c
kumax <inoribea@outlook.com>
2026-02-14 10:25:20 +0800

    Update SocialButton.js

--- themes/typography/components/SocialButton.js
+++ themes/typography/components/SocialButton.js
@@ -9,15 +9,6 @@ const SocialButton = () => {
   return (
     <div className='justify-center w-full md:justify-start md:w-52 flex-wrap flex my-2'>
       <div className='space-x-5  text-xl text-gray-600 dark:text-gray-400 text-center'>
-        {siteConfig('CONTACT_GITHUB') && (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            title={'github'}
-            href={siteConfig('CONTACT_GITHUB')}>
-            <i className='fab fa-github transform hover:scale-125 duration-150' />
-          </a>
-        )}
         {siteConfig('CONTACT_TWITTER') && (
           <a
             target='_blank'
@@ -34,33 +25,6 @@ const SocialButton = () => {
             href={siteConfig('CONTACT_TELEGRAM')}
             title={'telegram'}>
             <i className='fab fa-telegram transform hover:scale-125 duration-150' />
-          </a>
-        )}
-        {siteConfig('CONTACT_LINKEDIN') && (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            href={siteConfig('CONTACT_LINKEDIN')}
-            title={'linkedIn'}>
-            <i className='fab fa-linkedin transform hover:scale-125 duration-150' />
-          </a>
-        )}
-        {siteConfig('CONTACT_WEIBO') && (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            title={'weibo'}
-            href={siteConfig('CONTACT_WEIBO')}>
-            <i className='fab fa-weibo transform hover:scale-125 duration-150' />
-          </a>
-        )}
-        {siteConfig('CONTACT_INSTAGRAM') && (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            title={'instagram'}
-            href={siteConfig('CONTACT_INSTAGRAM')}>
-            <i className='fab fa-instagram transform hover:scale-125 duration-150' />
           </a>
         )}
         {siteConfig('CONTACT_EMAIL') && (
@@ -88,24 +52,6 @@ const SocialButton = () => {
             title={'bilibili'}
             href={siteConfig('CONTACT_BILIBILI')}>
             <i className='fab fa-bilibili transform hover:scale-125 duration-150' />
-          </a>
-        )}
-        {siteConfig('CONTACT_YOUTUBE') && (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            title={'youtube'}
-            href={siteConfig('CONTACT_YOUTUBE')}>
-            <i className='fab fa-youtube transform hover:scale-125 duration-150' />
-          </a>
-        )}
-        {siteConfig('CONTACT_THREADS') && (
-          <a
-            target='_blank'
-            rel='noreferrer'
-            title={'threads'}
-            href={siteConfig('CONTACT_THREADS')}>
-            <i className='fab fa-threads transform hover:scale-125 duration-150' />
           </a>
         )}
       </div>
